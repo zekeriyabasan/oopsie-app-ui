7dd710d2223d13f5cf0f107dae174eae6bacbd46
zekeriyabasan <zekeriyabasan@gmail.com>
2025-12-09 22:25:18 +0300

    creaeted: menu

---
+++ src/components/sidebar-components/menu.tsx
@@ -0,0 +1,13 @@
+import { Separator, Stack, Text } from "@chakra-ui/react";
+
+export const Menu = () => {
+  return (
+    <Stack>
+      <Text>Listem</Text>
+      <Separator />
+      <Text>Kullanıcılarım</Text>
+      <Separator />
+      <Text>Sohbetler</Text>
+    </Stack>
+  );
+};
