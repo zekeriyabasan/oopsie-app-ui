65772f97bee2f2d7bc4bf8462967fac659c018e3
zekeriyabasan <zekeriyabasan@gmail.com>
2025-12-09 22:41:30 +0300

    fix: layout avatar fix

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Text, VStack } from "@chakra-ui/react";
+import { Avatar, AvatarGroup, Box, Flex, HStack, Text } from "@chakra-ui/react";
 import { Menu } from "./components/sidebar-components/menu";
+
 
 export default function Layout() {
   return (
@@ -7,18 +8,21 @@ export default function Layout() {
       {/* SOL MENU */}
       <Box w="360px" borderRight="1px solid" p="4">
         {/* Kullanıcı Profili */}
-        <VStack mb="8">
-        
-        
-          <Text fontSize="lg" color={"black"} fontWeight="bold">
-            Zekeriya
-          </Text>
-          <Text fontSize="sm" color="gray.500">
-            Developer
-          </Text>
-        </VStack>
+        <HStack spaceX="3" mb={30} >
+          <AvatarGroup>
+            <Avatar.Root>
+              <Avatar.Image src="https://i.pravatar.cc/300" alt="profile" />
+              <Avatar.Fallback>AB</Avatar.Fallback>
+            </Avatar.Root>
+          </AvatarGroup>
 
-        {/* MENÜ */}
+          <HStack spaceX="1" flexDir="column" align="flex-start">
+            <Text fontWeight="bold" fontSize="sm">
+              Zekeriya Başan 
+            </Text>
+           
+          </HStack>
+        </HStack>
 
         <Menu />
       </Box>
